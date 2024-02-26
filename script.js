f41a8ea46c0dacf353adee0f1614e15115529bbc
TATU <157501375+TATARAAMKUMAR33@users.noreply.github.com>
2024-02-26 18:30:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let div = document.createElement("div");
+div.innerHTML = `<h1 id="header"></h1>`
+
+document.body.append(div);
+
+let callback = ()=>{
+    setTimeout(()=>document.getElementById("header").innerText = 10,1000);
+    setTimeout(()=>document.getElementById("header").innerText = 9,2000);
+    setTimeout(()=>document.getElementById("header").innerText = 8,3000);
+    setTimeout(()=>document.getElementById("header").innerText = 7,4000);
+    setTimeout(()=>document.getElementById("header").innerText = 6,5000);
+    setTimeout(()=>document.getElementById("header").innerText = 5,6000);
+    setTimeout(()=>document.getElementById("header").innerText = 4,7000);
+    setTimeout(()=>document.getElementById("header").innerText = 3,8000);
+    setTimeout(()=>document.getElementById("header").innerText = 2,9000);
+    setTimeout(()=>document.getElementById("header").innerText = 1,10000);
+    setTimeout(()=>document.getElementById("header").innerText = "Happy Independance Day",11000);
+}
+callback();
+
+
+
